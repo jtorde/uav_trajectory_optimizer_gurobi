a0932f3839146b25361c894cb1fa58dea9a37529
Parker Lusk <parkerclusk@gmail.com>
2019-04-24 18:05:41 -0400

    calc and plot attitude in matlab

--- src/project/matlab/plot_traj.m
+++ src/project/matlab/plot_traj.m
@@ -23,3 +23,92 @@ subplot(412); plot(t,v); grid on; ylabel('Velocity');
 subplot(413); plot(t,a); grid on; ylabel('Acceleration');
 subplot(414); plot(t,j); grid on; ylabel('Jerk');
 xlabel('Time [s]');
+
+figure(2), clf;
+hold on;
+for i = 1:length(t)
+    
+    % only plot at end of segments (dt rate)
+    if mod(i,dt/dc) ~=0, continue; end
+    
+    % calculate desired attitude from acceleration vector
+    R = computeAttitude([a(i,1);a(i,2);a(i,3)]);
+    
+    plotCoordinateFrame([p(i,1);p(i,2);p(i,3)],R,0.1)
+end
+xlabel('X'); ylabel('Y'); zlabel('Z');
+view(-30, 30); axis square; grid on;
+axis([-2 1 -1 1 -1 1]*3)
+pxmin = min(p(:,1))-0.25; pxmax = max(p(:,1))+0.25;
+pymin = min(p(:,2))-1; pymax = max(p(:,2))+1;
+pzmin = min(p(:,3))-0.25; pzmax = max(p(:,3))+0.25;
+axis([pxmin pxmax pymin pymax pzmin pzmax]);
+
+function R = computeAttitude(a)
+% Uses the acceleration vector to reconstruct the desired attitude
+% We assume a body flu coordinate frame
+
+    % we only care about accel dir
+    a = a/norm(a);
+
+    % find the axis of (positive) rotation
+    z = [0;0;1];
+    axis = cross(a,z);
+    axis = axis/norm(axis);
+    
+    % angle of rotation about the axis
+    angle = acos(dot(z,a));
+    
+    R = axisangle2rotm(axis, angle);
+end
+
+function plotCoordinateFrame(O, R, k)
+%PLOTCOORDINATEFRAME Plot a coordinate frame origin and orientation
+%   Coordinate frames have a origin and an orientation. This function draws
+%   the coordinate axes in a common frame.
+
+    % k     Size of each axis
+
+    if nargin == 1, R = eye(3); end
+
+    % Create coordinate axes starting at 0
+    kk = linspace(0,k,100);
+    CX = [kk; zeros(1,length(kk)); zeros(1,length(kk))];
+    CY = [zeros(1,length(kk)); kk; zeros(1,length(kk))];
+    CZ = [zeros(1,length(kk)); zeros(1,length(kk)); kk];
+    
+    % First rotate the coordinate frame from the local frame to the
+    % orientation of the desired frame in which we want to plot.
+    CX = R'*CX;
+    CY = R'*CY;
+    CZ = R'*CZ;
+    
+    % Then translate this frame to its origin
+    CX = repmat(O, 1, size(CX,2)) + CX;
+    CY = repmat(O, 1, size(CY,2)) + CY;
+    CZ = repmat(O, 1, size(CZ,2)) + CZ;
+    
+    % Plot the axes
+    hold on; ls = '-';
+    plot3(CX(1,:), CX(2,:), CX(3,:),'color','r','linewidth',2,'linestyle',ls);
+    plot3(CY(1,:), CY(2,:), CY(3,:),'color','g','linewidth',2,'linestyle',ls);
+    plot3(CZ(1,:), CZ(2,:), CZ(3,:),'color','b','linewidth',2,'linestyle',ls);
+end
+
+function R = axisangle2rotm(u,theta)
+    % make sure axis is normalized
+    u = u/norm(u);
+    ux = u(1); uy = u(2); uz = u(3);
+    
+    r11 = cos(theta) + ux*ux*(1-cos(theta));
+    r12 = ux*uy*(1-cos(theta)) - uz*sin(theta);
+    r13 = ux*uz*(1-cos(theta)) + uy*sin(theta);
+    r21 = uy*ux*(1-cos(theta)) + uz*sin(theta);
+    r22 = cos(theta) + uy*uy*(1-cos(theta));
+    r23 = uy*uz*(1-cos(theta)) - ux*sin(theta);
+    r31 = uz*ux*(1-cos(theta)) - uy*sin(theta);
+    r32 = uz*uy*(1-cos(theta)) + ux*sin(theta);
+    r33 = cos(theta) + uz*uz*(1-cos(theta));
+    
+    R = [r11 r12 r13; r21 r22 r23; r31 r32 r33];
+end
--- src/project/scripts/flipper.py
+++ src/project/scripts/flipper.py
@@ -18,131 +18,132 @@ from utils import *
 
 
 class Flipper:
-	"""Flipper"""
-	def __init__(self):
-		
-		self.sub_state = rospy.Subscriber('vicon', ViconState, self.state_cb)
+    """Flipper"""
+    def __init__(self):
+        
+        self.sub_state = rospy.Subscriber('vicon', ViconState, self.state_cb)
 
-		# trigger the flip
-		self.srv_flip = rospy.Service('flip', Trigger, self.flip_cb)
+        # trigger the flip
+        self.srv_flip = rospy.Service('flip', Trigger, self.flip_cb)
 
-		# outer loop setpoints
-		self.pub_goal = rospy.Publisher('goal', QuadGoal, queue_size=1)
+        # outer loop setpoints
+        self.pub_goal = rospy.Publisher('goal', QuadGoal, queue_size=1)
 
-		self.pub_drone_markers=rospy.Publisher('snapshots', MarkerArray, queue_size=10)
+        self.pub_drone_markers=rospy.Publisher('snapshots', MarkerArray, queue_size=10)
 
-		# initialize members
-		self.state_msg = ViconState()
+        # initialize members
+        self.state_msg = ViconState()
 
-		# desired control rate
-		self.dc = 0.01
+        # desired control rate
+        self.dc = 0.01
 
-		self.alt=1
-		
+        self.alt=1
+        
 
-	def flip_cb(self, req):
-		
-		success = self.generate_flip_trajectory()
+    def flip_cb(self, req):
+        
+        success = self.generate_flip_trajectory()
 
-		return TriggerResponse(success=success, message='')
-
-
-	def state_cb(self, msg):
-		self.state_msg = msg
+        return TriggerResponse(success=success, message='')
 
 
-	def generate_flip_trajectory(self):
-
-		# start optim at current pose
-		x0 = np.zeros((12,))
-		x0[0] = self.state_msg.pose.position.x
-		x0[1] = self.state_msg.pose.position.y
-		x0[2] = self.state_msg.pose.position.z
-
-		xf = np.zeros((12,))
-		xf = np.copy(x0)
-
-		s = Solver(JERK)
-		s.setInitialState(x0.tolist())
-		s.setFinalState(xf.tolist())
-		s.setMaxValues([50,50,50,100,100])  #Vel, accel, jerk, snap,...
-		s.setN(40)
-		s.setRadius(1)
-		s.solve()
-		self.pub_drone_markers.publish(getMarkerArray(GREEN,s.getAllPos(),s.getAllAccel()))
-
-		#
-		# Optimization Results
-		#
-
-		csvdata = []
-
-		# how many steps are in an optimization segment
-		K = int(s.dt/self.dc)
-
-		n = 0
-		while not rospy.is_shutdown() and n<s.N:
-
-			# publish each step at a uniform rate
-			rate = rospy.Rate(1.0/self.dc)
-
-			k = 0
-			while not rospy.is_shutdown() and k<K:
-
-				p, v, a, j = self.getHighRateGoal(s, n, k)
-
-				goal = QuadGoal()
-				goal.header.stamp = rospy.Time.now()
-				goal.pos.x,   goal.pos.y,   goal.pos.z   = p
-				goal.vel.x,   goal.vel.y,   goal.vel.z   = v
-				goal.accel.x, goal.accel.y, goal.accel.z = a
-				goal.jerk.x,  goal.jerk.y,  goal.jerk.z  = j
-				self.pub_goal.publish(goal)
-
-				csvdata.append(','.join(map(str,chain.from_iterable((p,v,a,j)))))
-
-				# maintain uniform timing (with period dc) for each intra-segment step
-				rate.sleep()
-				k += 1
-
-			# increment the segment number we are working with
-			n += 1
-
-		# Publish final state goal
-		goal = QuadGoal()
-		goal.header.stamp = rospy.Time.now()
-		goal.pos.x, goal.pos.y, goal.pos.z = xf[0:3]
-		goal.vel.x, goal.vel.y, goal.vel.z = xf[3:6]
-		goal.accel.x, goal.accel.y, goal.accel.z = xf[6:9]
-		goal.jerk.x, goal.jerk.y, goal.jerk.z = xf[9:12]
-		self.pub_goal.publish(goal)
-
-		with open("fliptraj.csv", "wb") as file:
-			for line in csvdata:
-				file.write(line)
-				file.write('\n')
-
-		return True
+    def state_cb(self, msg):
+        self.state_msg = msg
 
 
-	def getHighRateGoal(self, s, n, k):
-		# tau \in [0, dt]. Convert step index k to a time tau.
-		tau = k*self.dc
+    def generate_flip_trajectory(self):
 
-		p = tuple(s.getPos(n, tau, ii).getValue() for ii in range(3))
-		v = tuple(s.getVel(n, tau, ii).getValue() for ii in range(3))
-		a = tuple(s.getAccel(n, tau, ii).getValue() for ii in range(3))
-		j = tuple(s.getJerk(n, tau, ii).getValue() for ii in range(3))
+        # start optim at current pose
+        x0 = np.zeros((12,))
+        x0[0] = self.state_msg.pose.position.x
+        x0[1] = self.state_msg.pose.position.y
+        x0[2] = self.state_msg.pose.position.z
 
-		return p, v, a, j
+        xf = np.zeros((12,))
+        xf = np.copy(x0)
+
+        s = Solver(JERK)
+        s.setInitialState(x0.tolist())
+        s.setFinalState(xf.tolist())
+        s.setMaxValues([50,50,50,100,100])  #Vel, accel, jerk, snap,...
+        s.setN(40)
+        s.setRadius(1)
+        s.solve()
+        self.pub_drone_markers.publish(getMarkerArray(GREEN,s.getAllPos(),s.getAllAccel()))
+
+        #
+        # Optimization Results
+        #
+
+        csvdata = []
+
+        # how many steps are in an optimization segment
+        K = int(s.dt/self.dc)
+
+        n = 0
+        while not rospy.is_shutdown() and n<s.N:
+
+            # publish each step at a uniform rate
+            rate = rospy.Rate(1.0/self.dc)
+
+            k = 0
+            while not rospy.is_shutdown() and k<K:
+
+                p, v, a, j = self.getHighRateGoal(s, n, k)
+
+                goal = QuadGoal()
+                goal.header.stamp = rospy.Time.now()
+                goal.pos.x,   goal.pos.y,   goal.pos.z   = p
+                goal.vel.x,   goal.vel.y,   goal.vel.z   = v
+                goal.accel.x, goal.accel.y, goal.accel.z = a
+                goal.jerk.x,  goal.jerk.y,  goal.jerk.z  = j
+                goal.xy_mode = goal.z_mode = 0
+                self.pub_goal.publish(goal)
+
+                csvdata.append(','.join(map(str,chain.from_iterable((p,v,a,j)))))
+
+                # maintain uniform timing (with period dc) for each intra-segment step
+                rate.sleep()
+                k += 1
+
+            # increment the segment number we are working with
+            n += 1
+
+        # Publish final state goal
+        goal = QuadGoal()
+        goal.header.stamp = rospy.Time.now()
+        goal.pos.x, goal.pos.y, goal.pos.z = xf[0:3]
+        goal.vel.x, goal.vel.y, goal.vel.z = xf[3:6]
+        goal.accel.x, goal.accel.y, goal.accel.z = xf[6:9]
+        goal.jerk.x, goal.jerk.y, goal.jerk.z = xf[9:12]
+        self.pub_goal.publish(goal)
+
+        with open("fliptraj.csv", "wb") as file:
+            for line in csvdata:
+                file.write(line)
+                file.write('\n')
+
+        return True
+
+
+    def getHighRateGoal(self, s, n, k):
+        # tau \in [0, dt]. Convert step index k to a time tau.
+        tau = k*self.dc
+
+        p = tuple(s.getPos(n, tau, ii).getValue() for ii in range(3))
+        v = tuple(s.getVel(n, tau, ii).getValue() for ii in range(3))
+        a = tuple(s.getAccel(n, tau, ii).getValue() for ii in range(3))
+        j = tuple(s.getJerk(n, tau, ii).getValue() for ii in range(3))
+
+        return p, v, a, j
 
 
 if __name__ == '__main__':
-	rospy.init_node('flipper', anonymous=False)
+    rospy.init_node('flipper', anonymous=False)
 
-	try:
-		obj = Flipper()
+    try:
+        obj = Flipper()
 
-		rospy.spin()
-	except rospy.ROSInterruptException:
-		pass
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
